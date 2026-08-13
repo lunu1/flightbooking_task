b72fa4a4d9116ec91b89c8d0fae95023041d111e
Lufna Nasrin T <nasrinlufna@gmail.com>
2026-08-14 00:01:20 +0400

    Add Express server with a testing endpoing /health

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok'});
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
