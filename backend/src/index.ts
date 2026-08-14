3fdc00d99d472dc7b91313ef16cf5ef4fed7fcd3
Lufna Nasrin T <nasrinlufna@gmail.com>
2026-08-14 17:32:39 +0400

    registration and tesstin g success

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import pool from "./config/db";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use("/auth", authRoutes);
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
